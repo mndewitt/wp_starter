a4085132d3ef7b33e27a514e6054246654b3194a
Mike DeWitt <mdewitt07@gmail.com>
2015-06-03 14:44:05 -0400

    add variables for easy customization

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
-#install core and setup database
+#!/bin/bash
+
+#declare some variables for easy customization
+DB_NAME=localwp
+
+SITE_TITLE=dev
+
+URL=localhost/shellpractice
+
+ADMIN_EMAIL=mdewitt07@gmail.com
+
+
+#install core and database
 wp core download
-wp core config --dbname=localwp --dbuser=root --dbpass=root
+wp core config --dbname=${DB_NAME} --dbuser=root --dbpass=root
 wp db create
-wp core install --title=dev --admin_user=admin --admin_password=1 --url=localhost/shellpractice --admin_email=mdewitt07@gmail.com
+wp core install --title=${SITE_TITLE} --admin_user=admin --admin_password=1 --url=${URL} --admin_email=${ADMIN_EMAIL}
 
 #install my bones fork
 wp theme install https://github.com/mndewitt/bones/archive/master.zip --activate
@@ -17,3 +29,6 @@ wp plugin install wordpress-seo --activate
 #rewrite permalinks for pretty urls
 wp rewrite structure '/%postname%'  --hard
 wp rewrite flush --hard
+
+#shout out upon completion
+echo 'WordPress successfully installed'
