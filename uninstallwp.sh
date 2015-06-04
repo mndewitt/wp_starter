7478ef3726c2c815312770870d82ac14b6431b46
Mike DeWitt <mdewitt07@gmail.com>
2015-06-03 21:27:05 -0400

    add wp-cli.yml to delete exceptions

--- uninstallwp.sh
+++ uninstallwp.sh
@@ -5,7 +5,7 @@ wp db drop
 
 #delete everything except the shell scripts
 shopt -s extglob
-rm -rf !(installwp.sh|uninstallwp.sh)
+rm -rf !(installwp.sh|uninstallwp.sh|wp-cli.yml)
 
 #shout out upon completion
 echo 'WordPress uninstalled successfully'
